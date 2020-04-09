ba152e8ab97437da8908992a6b8435f92e9f89a7
wlhuang <807948299@qq.com>
2020-04-10 00:41:54 +0800

    use orm insert data to database table

--- src/blog/controllers/testModel.go
+++ src/blog/controllers/testModel.go
@@ -2,16 +2,25 @@ package controllers
 
 import (
 	"fmt"
+	"hwl/tool/logs"
 
 	"github.com/astaxie/beego"         // beego的web端
 	"github.com/astaxie/beego/orm"     // 操作mysql的
 	_ "github.com/go-sql-driver/mysql" // 驱动
 )
 
-// UserInfo .
+func init() {
+	// 别名连接数据库只能调用一次，再次调用会报错defaulty已经注册不能reuse
+	// testmodel是操作的数据库名 mysql是使用的驱动名 default是连接数据库的一个别名
+	orm.RegisterDataBase("default", "mysql", "root:@tcp(127.0.0.1:3306)/testmodel?charset=utf8", 30)
+	orm.RegisterModel(new(UserInfo)) // 创建一个user_info表
+}
+
+// UserInfo 这个结构体要和数据库表的名字（user_info）对应
+//（数据库会自动把大写的变成小写并增加下划线）
 type UserInfo struct {
 	ID       int64
-	UserName string
+	Username string
 	Passwd   string
 }
 
@@ -22,16 +31,13 @@ type TestModelController struct {
 
 // Get .
 func (c *TestModelController) Get() {
-	// db, err = sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/hwlDB?charset=utf8")
-	orm.RegisterDataBase("default", "mysql", "root:@tcp(127.0.0.1:3306)/testmodel?charset=utf8", 30)
-
-	orm.RegisterModel(new(UserInfo)) // 创建一个user_info表 （数据库会自动把大写的变成小写并增加下划线）
-
 	o := orm.NewOrm()
-	user := UserInfo{UserName: "张三", Passwd: "123456"}
-	id, _ := o.Insert(&user)
+	user := UserInfo{Username: "hwl", Passwd: "123456"}
+	id, err := o.Insert(&user) // 想要插入成功，需要事先在数据库里面创建表user_info
+	if err != nil {
+		logs.Println(err)
+		return
+	}
 
-	//c.TplName = "index.html"
 	c.Ctx.WriteString(fmt.Sprintf("操作数据库id:%v", id))
-
 }
