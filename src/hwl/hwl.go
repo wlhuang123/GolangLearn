8310d1f46b530803e182ac5dc874611397cedb5d
wlhuang <807948299@qq.com>
2020-04-12 08:46:36 +0800

    统计代码耗时的小方法

--- src/hwl/hwl.go
+++ src/hwl/hwl.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	learn.ActomicTest()
+	learn.TimeTest()
 }
---
+++ src/hwl/learn/time.go
@@ -0,0 +1,24 @@
+package learn
+
+import (
+	"hwl/tool/logs"
+	"time"
+)
+
+/*
+  时间使用相关的
+*/
+
+// TimeTest .
+func TimeTest() {
+	calculateUseTime()
+}
+
+// calculateUseTime 统计耗时
+func calculateUseTime() {
+	begin := time.Now()
+	time.Sleep(1 * time.Second)
+	end := time.Now()
+
+	logs.Println(end.Sub(begin))
+}
