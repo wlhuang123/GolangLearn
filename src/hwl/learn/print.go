fc1c7a4620db34ed0ef0422910d1d0e67aa3f75c
wlhuang <807948299@qq.com>
2020-04-19 16:45:07 +0800

    learn print format %q %s %v

--- src/hwl/learn/print.go
+++ src/hwl/learn/print.go
@@ -15,7 +15,9 @@ type print struct {
 func PrintTest() {
 	var p print
 	p.structValue().
-		porinter()
+		porinter().
+		string().
+		println()
 }
 
 func (p *print) structValue() *print {
@@ -32,5 +34,25 @@ func (p *print) structValue() *print {
 func (p *print) porinter() *print {
 	var v *int
 	logs.Printf("%p", v)
+	return p
+}
+
+func (p *print) string() *print {
+	s := "print learn"
+	logs.Printf("%s", s)
+	logs.Printf("%q", s) // %q打印字符串类型相比%s会增加双引号打印
+
+	c := 'a'
+	logs.Printf("%q", c) // %q还可以打印单引号的字符，%s打印会乱码
+
+	return p
+}
+
+func (p *print) println() *print {
+	s := "print learn"
+	// %v是内置格式的任意值
+	// Println就是默认用%v的方式来打印
+	logs.Printf("%v", s)
+
 	return p
 }
