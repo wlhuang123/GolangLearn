46226166881970a33696480f849b7b4953fced61
wlhuang <807948299@qq.com>
2020-06-25 09:14:33 +0800

    test benchmark case

--- src/hwl/test/benchmark_test.go
+++ src/hwl/test/benchmark_test.go
@@ -1,11 +1,41 @@
 package test
 
 import (
-	"hwl/tool/logs"
+	"bytes"
+	"fmt"
+	"strings"
 	"testing"
 )
 
-// BenchmarkAdd .
-func BenchmarkAdd(b *testing.B) {
-	logs.Println("hwl BenchMark:", b.N)
+// fmt.Printf
+func BenchmarkFmtSprintfMore(b *testing.B) {
+	var s string
+	for i := 0; i < b.N; i++ {
+		s += fmt.Sprintf("%s%s", "hello", "world")
+	}
+}
+
+// 加号 拼接
+func BenchmarkAddMore(b *testing.B) {
+	var s string
+	for i := 0; i < b.N; i++ {
+		s += "hello" + "world"
+	}
+}
+
+// strings.Join
+func BenchmarkStringsJoinMore(b *testing.B) {
+	var s string
+	for i := 0; i < b.N; i++ {
+		s += strings.Join([]string{"hello", "world"}, "")
+	}
+}
+
+// bytes.Buffer
+func BenchmarkBufferMore(b *testing.B) {
+	buffer := bytes.Buffer{}
+	for i := 0; i < b.N; i++ {
+		buffer.WriteString("hello")
+		buffer.WriteString("world")
+	}
 }
