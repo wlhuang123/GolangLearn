6a554b4973587daae767326c14c0f548b74236b5
wlhuang <807948299@qq.com>
2020-09-28 07:07:17 +0800

    237 delete_elemets

--- src/hwl/hwl.go
+++ src/hwl/hwl.go
@@ -3,7 +3,7 @@ package main
 import "hwl/leetcode"
 
 func main() {
-	leetcode.Test219()
-	//leetcode.GenBasicCode("219", "contains_repeat_elementII")
+	leetcode.Test237()
+	// leetcode.GenBasicCode("237", "delete_element")
 
 }
